d9e30b3e9a1804425d8e7a46376776e565767785
Ammar The Trainer <Ammarthetrainer@gmail.com>
2018-10-27 12:42:02 +0500

    Project Created

---
+++ Ch12-P2-LinqRetValues/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch12_P2_LinqRetValues
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
