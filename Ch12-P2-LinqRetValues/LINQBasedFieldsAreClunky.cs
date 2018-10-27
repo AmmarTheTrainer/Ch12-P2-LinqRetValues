74c26fb4bb3440a573cb98029048c3db6938b0cf
Ammar The Trainer <Ammarthetrainer@gmail.com>
2018-10-27 13:04:51 +0500

    Update Ch12-P2-LinqRetValues/LINQBasedFieldsAreClunky.cs

--- Ch12-P2-LinqRetValues/LINQBasedFieldsAreClunky.cs
+++ Ch12-P2-LinqRetValues/LINQBasedFieldsAreClunky.cs
@@ -8,6 +8,19 @@ namespace Ch12_P2_LinqRetValues
 {
     class LINQBasedFieldsAreClunky
     {
-
+        private static string[] currentVideoGames = {"Morrowind", "Uncharted 2", "Fallout 3", "Daxter", "System Shock 2"};
+        
+        // Can't use implicit typing here! Must know type of subset!
+        private IEnumerable<string> subset = from g in currentVideoGames
+                                             where g.Contains(" ")
+                                             orderby g
+                                             select g;
+        public void PrintGames()
+        {
+            foreach (var item in subset)
+            {
+                Console.WriteLine(item);
+            }
+        }
     }
 }
