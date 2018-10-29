a432db49879b05ea298cf55e9d430d5adab10bd5
Ammar The Trainer <Ammarthetrainer@gmail.com>
2018-10-29 16:20:15 +0500

    final update

--- Ch12-P2-LinqRetValues/Program.cs
+++ Ch12-P2-LinqRetValues/Program.cs
@@ -10,7 +10,57 @@ namespace Ch12_P2_LinqRetValues
     {
         static void Main(string[] args)
         {
+            #region Internet Surfing
 
+            #region Return a Query from a Method
+
+
+
+            #endregion
+
+            #endregion
+
+            Console.WriteLine("***** LINQ Return Values *****\n");
+
+            //IEnumerable<string> subset = GetStringSubset();
+
+            //foreach (string item in subset)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            #region Returning LINQ Results via Immediate Execution
+
+            string[] methodResult = GetStringSubsetAsArray();
+
+            foreach (string item in methodResult)
+            {
+                Console.WriteLine(item);
+            }
+
+            #endregion
+
+            Console.ReadLine();
+        }
+
+        static string[] GetStringSubsetAsArray()
+        {
+            string[] colors = { "Light Red", "Green", "Yellow", "Dark Red", "Red", "Purple" };
+
+            //IEnumerable<string> theRedColors = (from c in colors where c.Contains("Red") select c).ToArray();
+             string[] theRedColors = (from c in colors where c.Contains("Red") select c).ToArray();
+            
+            // Map results into an array.
+            //return theRedColors.ToArray();
+            return theRedColors;
+        }
+        
+        private static IEnumerable<string> GetStringSubset()
+        {
+            string[] colors = { "Light Red", "Green", "Yellow", "Dark Red", "Red", "Purple" };
+            // Note subset is an IEnumerable<string>-compatible object.
+            IEnumerable<string> theRedColors = from c in colors where c.Contains("Red") select c;
+            return theRedColors;
         }
     }
 }
---
+++ Ch12-P2-LinqRetValues/ReturnQueryFromMethodClass.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch12_P2_LinqRetValues
+{
+    class ReturnQueryFromMethodClass
+    {
+        // QueryMethhod1 returns a query as its value.
+        IEnumerable<string> QueryMethod1(ref int[] ints)
+        {
+            IEnumerable<string> intsToStrings = from i in ints
+                                                where i > 4
+                                                select i.ToString();
+            return intsToStrings;
+        }
+
+        // QueryMethod2 returns a query as the value of parameter returnQ.
+        void QueryMethod2(ref int[] ints, out IEnumerable<string> returnQ)
+        {
+            IEnumerable<string> intsToStrings = from i in ints
+                                                where i < 4
+                                                select i.ToString();
+            returnQ = intsToStrings;
+        }
+    }
+}
